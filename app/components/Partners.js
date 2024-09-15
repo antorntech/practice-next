015fae796cb3668c0f104275b517cdd7acf54629
AM Antor <creativefreelancerantor@gmail.com>
2024-09-15 19:10:15 +0600

    add some change

---
+++ app/components/Partners.js
@@ -0,0 +1,9 @@
+"use client";
+
+import React from "react";
+
+const Partners = () => {
+  return <div className="partners"></div>;
+};
+
+export default Partners;
--- app/page.js
+++ app/page.js
@@ -1,11 +1,15 @@
 import Image from "next/image";
 import heroBg from "@/app/assets/hero.png";
+import Partners from "./components/Partners";
 
 export default function Home() {
   return (
     <main className="w-full">
       <div className="text-3xl font-bold">
         <Image src={heroBg} alt="hero" />
+      </div>
+      <div>
+        <Partners />
       </div>
     </main>
   );
