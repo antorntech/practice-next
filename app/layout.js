b054a84c069cb5ca01416962ce01d8a3d0d9a67f
AM Antor <creativefreelancerantor@gmail.com>
2024-09-15 19:02:20 +0600

    add new header

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
 import localFont from "next/font/local";
 import "./globals.css";
 import Link from "next/link";
+import logo from "@/app/assets/logo.png";
+import Image from "next/image";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -31,29 +33,37 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={sailecThin.className}>
-        <nav className="flex justify-between items-center bg-slate-400 p-5">
-          <h1 className="font-bold">
-            <Link href="/" className="text-lg font-bold">
-              My App
-            </Link>
-          </h1>
-          <ul className="flex items-center gap-5">
+      <body className={sailecMedium.className}>
+        <nav className="max-w-screen-lg mx-auto flex items-center justify-between py-2">
+          <ul className="flex items-center gap-5 text-black">
             <li>
-              <Link href="/">Home</Link>
+              <Link href="/">About Us</Link>
             </li>
             <li>
-              <Link href="/about">About</Link>
+              <Link href="/about">Discover Malta</Link>
             </li>
             <li>
-              <Link href="/blogs">Blogs</Link>
+              <Link href="/blogs">Fleet</Link>
+            </li>
+          </ul>
+          <div>
+            <Image src={logo} alt="logo" />
+          </div>
+          <ul className="flex items-center gap-5 text-black">
+            <li>
+              <Link href="/">Long-Term Lease</Link>
             </li>
             <li>
-              <Link href="/posts">Posts</Link>
+              <Link href="/about">Contact Us</Link>
+            </li>
+            <li>
+              <button className="px-5 py-2 bg-gradient-to-r from-[#9D0208] to-[#D00000] text-white rounded-md shadow-md font-bold">
+                Book Now
+              </button>
             </li>
           </ul>
         </nav>
-        <div className="p-5">{children}</div>
+        <div>{children}</div>
       </body>
     </html>
   );
--- app/page.js
+++ app/page.js
@@ -1,7 +1,12 @@
+import Image from "next/image";
+import heroBg from "@/app/assets/hero.png";
+
 export default function Home() {
   return (
-    <main className="w-full h-[80vh] flex items-center justify-center">
-      <div className="text-3xl font-bold">Hello World!</div>
+    <main className="w-full">
+      <div className="text-3xl font-bold">
+        <Image src={heroBg} alt="hero" />
+      </div>
     </main>
   );
 }
